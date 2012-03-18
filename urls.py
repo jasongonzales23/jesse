da1ff6cd4deb5691fee308042945135b93e63942
Jason Gonzales <jason.gonzales23@gmail.com>
2012-03-18 14:27:05 -0400

    first push to prod

---
+++ urls.py
@@ -0,0 +1,33 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'jesse.views.home', name='home'),
+    # url(r'^jesse/', include('jesse.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.contrib.staticfiles.views',
+        url(r'^static/(?P<path>.*)$', 'serve'),
+    )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    url(r'', include('django.contrib.staticfiles.urls')),
+    ) 
