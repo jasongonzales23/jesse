aac411efbed803741add3d9a7de43b21f4b2551b
Jason Gonzales <jason.gonzales23@gmail.com>
2012-04-01 22:17:16 -0400

    hooking up more thumbs and stuff

--- static_tmp/js/script.js
+++ static_tmp/js/script.js
@@ -11,9 +11,13 @@ var gallery = function(element){
         this.element = $(element);
         this.images = this.element.find('.image');
         this.galleryLength = this.images.length;
-        this.thumbnails = this.element.find('.thumb');
         this.prev = this.element.find('.prev');
         this.next = this.element.find('.next');
+        this.thumbnails = this.element.find('.thumb');
+        this.thumbStrip = this.element.find('.thumbs');
+        this.thumbPrev = this.element.find('.thumb-prev');
+        this.thumbNext = this.element.find('.thumb-next');
+        this.thumbControlOffset = 16;
         this.images.hide();
         this.showImages();
         this.bindButtons();
@@ -32,6 +36,20 @@ var gallery = function(element){
         });
     };
     
+    this.bindThumbButtons = function(){
+        var that = this;
+        
+        this.thumbNext.on('click', function(){
+            this.currentThumbOffset
+            this.navThumb();
+        });
+        
+        this.thumbPrev.on('click', function(){
+            this.navThumb();
+        });
+        
+    };
+    
     this.showImages = function(){
         if (this.i < 0 ){
             this.i = this.galleryLength -1;
@@ -44,8 +62,29 @@ var gallery = function(element){
         this.thumbnails.removeClass('active');
         this.images.eq(this.i).show();
         this.thumbnails.eq(this.i).addClass('active');
-        console.log(this.i);
+        this.selectThumb();
     };
+    
+    this.selectThumb = function(){
+        var that = this;
+        this.thumbStrip.animate(
+            {'marginTop': (this.i*-65) + this.thumbControlOffset + 'px'}, 300, function(){
+                that.currentThumbOffset = that.thumbStrip.css('marginTop');
+                console.log(that.currentThumbOffset);
+        });
+
+    };
+    
+    this.navThumb = function(){
+        this.currentThumbOffset = this.thumbStrip.css('marginTop');
+        console.log(this.currentThumbOffset);
+        this.thumbStrip.animate(
+            {'marginTop': (this.i*-65) + this.thumbControlOffset + 'px'}, 300, function(){
+                
+            }
+        );
+    };
+    
 };
 
 
